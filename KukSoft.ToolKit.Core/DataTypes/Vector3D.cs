6af552bc7f0ab067c99348e59a454e1fc0987181
Sergej Kukshaus <kukshaus@dial.de>
2018-05-29 15:40:19 +0200

    Vector2D implementierung

---
+++ KukSoft.ToolKit.Core/DataTypes/Vector3D.cs
@@ -0,0 +1,9 @@
+﻿namespace KukSoft.ToolKit.DataTypes
+{
+    public struct Vector3D
+    {
+        public double X { get; }
+        public double Y { get; }
+        public double Z { get; }
+    }
+}
---
+++ KukSoft.ToolKit.Core/DataTypes/Vector4D.cs
@@ -0,0 +1,11 @@
+﻿namespace KukSoft.ToolKit.DataTypes
+{
+    public struct Vector4D
+    {
+        public double W { get; }
+        public double X { get; }
+        public double Y { get; }
+        public double Z { get; }
+
+    }
+}
