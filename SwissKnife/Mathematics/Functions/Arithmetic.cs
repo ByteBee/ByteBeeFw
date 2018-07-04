e90e6e590b5344315f78e25767bedfb2ab0cece1
Sergej Kukshaus <kukshaus@dial.de>
2018-07-04 12:01:31 +0200

    factorial and semifactorial functions added

--- SwissKnife/Mathematics/Functions/Arithmetic.cs
+++ SwissKnife/Mathematics/Functions/Arithmetic.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using static SwissKnife.Fancy;
 
 namespace SwissKnife.Mathematics.Functions
 {
@@ -197,61 +198,182 @@ namespace SwissKnife.Mathematics.Functions
         /// Returns <b>e</b> raised to the specified power.
         /// </summary>
         double Exp(double d);
+
+        long Fact(int n);
+        long FactD(int n);
+
     }
 
     internal partial class MathFunctions : IMathFunctions
     {
         public decimal Ceiling(decimal d) => Math.Ceiling(d);
+
         public double Ceiling(double a) => Math.Ceiling(a);
+
         public decimal Floor(decimal d) => Math.Floor(d);
+
         public double Floor(double d) => Math.Floor(d);
 
         public double Round(double value) => Math.Round(value);
+
         public double Round(double value, int digits) => Math.Round(value, digits);
+
         public double Round(double value, MidpointRounding mode) => Math.Round(value, mode);
+
         public double Round(double value, int digits, MidpointRounding mode) => Math.Round(value, digits, mode);
+
         public decimal Round(decimal value) => Math.Round(value);
+
         public decimal Round(decimal value, int decimals) => Math.Round(value, decimals);
+
         public decimal Round(decimal value, MidpointRounding mode) => Math.Round(value, mode);
+
         public decimal Round(decimal value, int decimals, MidpointRounding mode) => Math.Round(value, decimals, mode);
+
         public double Round1(double value) => Math.Round(value, 1);
+
         public double Round1(double value, MidpointRounding mode) => Math.Round(value, 1, mode);
+
         public decimal Round1(decimal value) => Math.Round(value, 1);
+
         public decimal Round1(decimal value, MidpointRounding mode) => Math.Round(value, 1, mode);
+
         public double Round2(double value) => Math.Round(value, 2);
+
         public double Round2(double value, MidpointRounding mode) => Math.Round(value, 2, mode);
+
         public decimal Round2(decimal value) => Math.Round(value, 2);
+
         public decimal Round2(decimal value, MidpointRounding mode) => Math.Round(value, 2, mode);
+
         public double Round3(double value) => Math.Round(value, 3);
+
         public double Round3(double value, MidpointRounding mode) => Math.Round(value, 3, mode);
+
         public decimal Round3(decimal value) => Math.Round(value, 3);
+
         public decimal Round3(decimal value, MidpointRounding mode) => Math.Round(value, 3, mode);
 
         public sbyte Abs(sbyte value) => Math.Abs(value);
+
         public short Abs(short value) => Math.Abs(value);
+
         public int Abs(int value) => Math.Abs(value);
+
         public long Abs(long value) => Math.Abs(value);
+
         public float Abs(float value) => Math.Abs(value);
+
         public double Abs(double value) => Math.Abs(value);
+
         public decimal Abs(decimal value) => Math.Abs(value);
 
         public int Sign(sbyte value) => Math.Sign(value);
+
         public int Sign(short value) => Math.Sign(value);
+
         public int Sign(int value) => Math.Sign(value);
+
         public int Sign(long value) => Math.Sign(value);
+
         public int Sign(float value) => Math.Sign(value);
+
         public int Sign(double value) => Math.Sign(value);
+
         public int Sign(decimal value) => Math.Sign(value);
 
         public double LogE(double d) => Math.Log(d);
+
         public double Log2(double d) => Math.Log(d, 2);
+
         public double Log10(double d) => Math.Log10(d);
+
         public double Log(double a, double @base) => Math.Log(a, @base);
+
         public double Exp(double d) => Math.Exp(d);
+
         public double Pow(double x, double @base) => Math.Pow(x, @base);
+
         public double Pow2(double x) => Math.Pow(x, 2);
+
         public double Pow8(double x) => Math.Pow(x, 8);
+
         public double Pow10(double x) => Math.Pow(x, 10);
+
         public double Pow16(double x) => Math.Pow(x, 16);
+
+        private readonly long[] _factValues =
+        {
+            /*  0 */ 1,
+            /*  1 */ 1,
+            /*  2 */ 2,
+            /*  3 */ 6,
+            /*  4 */ 24,
+            /*  5 */ 120,
+            /*  6 */ 720,
+            /*  7 */ 5040,
+            /*  8 */ 40320,
+            /*  9 */ 362880,
+            /* 10 */ 3628800,
+            /* 11 */ 39916800,
+            /* 12 */ 479001600,
+            /* 13 */ 6227020800,
+            /* 14 */ 87178291200,
+            /* 15 */ 1307674368000,
+            /* 16 */ 20922789888000,
+            /* 17 */ 355687428096000,
+            /* 18 */ 6402373705728000,
+            /* 19 */ 121645100408832000,
+            /* 20 */ 2432902008176640000
+        };
+        public long Fact(int n)
+        {
+            guard.AgainstOutOfRange(n, "fact(n)", 0, 20);
+            return _factValues[n];
+        }
+
+        private readonly long[] _doubleFactValues =
+        {
+            /*  0 */ 1,
+            /*  1 */ 1,
+            /*  2 */ 2,
+            /*  3 */ 3,
+            /*  4 */ 8,
+            /*  5 */ 15,
+            /*  6 */ 48,
+            /*  7 */ 105,
+            /*  8 */ 384,
+            /*  9 */ 945,
+            /* 10 */ 3840,
+            /* 11 */ 10395,
+            /* 12 */ 46080,
+            /* 13 */ 135135,
+            /* 14 */ 645120,
+            /* 15 */ 2027025,
+            /* 16 */ 10321920,
+            /* 17 */ 34459425,
+            /* 18 */ 185794560,
+            /* 19 */ 654729075,
+            /* 20 */ 3715891200,
+            /* 21 */ 13749310575,
+            /* 22 */ 81749606400,
+            /* 23 */ 316234143225,
+            /* 24 */ 1961990553600,
+            /* 25 */ 7905853580625,
+            /* 26 */ 51011754393600,
+            /* 27 */ 213458046676875,
+            /* 28 */ 1428329123020800,
+            /* 29 */ 6190283353629370,
+            /* 30 */ 42849873690624000,
+            /* 31 */ 191898783962511000,
+            /* 32 */ 1371195958099970000,
+            /* 33 */ 6332659870762850000,
+        };
+        public long FactD(int n)
+        {
+            guard.AgainstOutOfRange(n, "factd(n)", 0, 33);
+            return _doubleFactValues[n];
+        }
+
     }
 }
