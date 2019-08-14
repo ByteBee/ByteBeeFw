529892da331130fb04232124bc32e19303441b85
Sergej Kukshaus <kukshaus@dial.de>
2019-08-15 00:12:18 +0200

    added trig functions based on angle units

--- ByteBee/Numerics/Trig.cs
+++ ByteBee/Numerics/Trig.cs
@@ -31,11 +31,29 @@ namespace ByteBee.Core.Numerics
         }
 
         /// <summary>
+        /// cosine
+        /// </summary>
+        public static double Cos(double theta, AngleUnit angle)
+        {
+            theta *= angle.ToRad();
+            return Cos(theta);
+        }
+
+        /// <summary>
         /// tangent (rad)
         /// </summary>
         public static double Tan(double theta)
         {
             return Math.Tan(theta);
+        }
+
+        /// <summary>
+        /// tangent
+        /// </summary>
+        public static double Tan(double theta, AngleUnit angle)
+        {
+            theta *= angle.ToRad();
+            return Tan(theta);
         }
 
         /// <summary>
@@ -47,11 +65,29 @@ namespace ByteBee.Core.Numerics
         }
 
         /// <summary>
+        /// cosecant
+        /// </summary>
+        public static double Csc(double theta, AngleUnit angle)
+        {
+            theta *= angle.ToRad();
+            return Csc(theta);
+        }
+
+        /// <summary>
         /// secant (rad)
         /// </summary>
         public static double Sec(double theta)
         {
             return 1 / Math.Cos(theta);
+        }
+
+        /// <summary>
+        /// secant
+        /// </summary>
+        public static double Sec(double theta, AngleUnit angle)
+        {
+            theta *= angle.ToRad();
+            return Sec(theta);
         }
 
         /// <summary>
@@ -63,47 +99,11 @@ namespace ByteBee.Core.Numerics
         }
 
         /// <summary>
-        /// cosine
-        /// </summary>
-        public static double Cos(double theta, AngleUnit angle)
-        {
-            theta *= angle.ToRad();
-            return Cos(theta);
-        }
-
-        /// <summary>
-        /// tangent
-        /// </summary>
-        public static double Tan(double theta, AngleUnit unit)
-        {
-            theta *= unit.ToRad();
-            return Tan(theta);
-        }
-
-        /// <summary>
-        /// cosecant
-        /// </summary>
-        public static double Csc(double theta, AngleUnit unit)
-        {
-            theta *= unit.ToRad();
-            return Csc(theta);
-        }
-
-        /// <summary>
-        /// secant
-        /// </summary>
-        public static double Sec(double theta, AngleUnit unit)
-        {
-            theta *= unit.ToRad();
-            return Sec(theta * unit.ToRad());
-        }
-
-        /// <summary>
         /// cotangent
         /// </summary>
-        public static double Cot(double theta, AngleUnit unit)
+        public static double Cot(double theta, AngleUnit angle)
         {
-            theta *= unit.ToRad();
+            theta *= angle.ToRad();
             return Cot(theta);
         }
 
@@ -117,6 +117,14 @@ namespace ByteBee.Core.Numerics
         }
 
         /// <summary>
+        /// arcus sine
+        /// </summary>
+        public static double Asin(double theta, AngleUnit angle)
+        {
+            return Asin(theta) / angle.ToRad();
+        }
+
+        /// <summary>
         /// arcus cosine (rad)
         /// </summary>
         public static double Acos(double theta)
@@ -125,11 +133,26 @@ namespace ByteBee.Core.Numerics
         }
 
         /// <summary>
+        /// arcus cosine
+        /// </summary>
+        public static double Acos(double theta, AngleUnit angle)
+        {
+            return Acos(theta) / angle.ToRad();
+        }
+        /// <summary>
         /// arcus tangent (rad)
         /// </summary>
         public static double Atan(double theta)
         {
             return Math.Atan(theta);
+        }
+
+        /// <summary>
+        /// arcus tangent
+        /// </summary>
+        public static double Atan(double theta, AngleUnit angle)
+        {
+            return Atan(theta) / angle.ToRad();
         }
 
         /// <summary>
@@ -141,11 +164,27 @@ namespace ByteBee.Core.Numerics
         }
 
         /// <summary>
+        /// arcus cosecant
+        /// </summary>
+        public static double Acsc(double theta, AngleUnit angle)
+        {
+            return Acsc(theta) / angle.ToRad();
+        }
+
+        /// <summary>
         /// arcus secant (rad)
         /// </summary>
         public static double Asec(double theta)
         {
             return 2 * Atan(1) - Atan(Math.Sign(theta) / Math.Sqrt(theta * theta - 1));
+        }
+
+        /// <summary>
+        /// arcus secant
+        /// </summary>
+        public static double Asec(double theta, AngleUnit angle)
+        {
+            return Asec(theta) / angle.ToRad();
         }
 
         /// <summary>
@@ -157,51 +196,11 @@ namespace ByteBee.Core.Numerics
         }
 
         /// <summary>
-        /// arcus sine
-        /// </summary>
-        public static double Asin(double theta, AngleUnit angle)
-        {
-            return Asin(theta) / angle.ToRad();
-        }
-
-        /// <summary>
-        /// arcus cosine
-        /// </summary>
-        public static double Acos(double theta, AngleUnit unit)
-        {
-            return Acos(theta) / unit.ToRad();
-        }
-
-        /// <summary>
-        /// arcus tangent
-        /// </summary>
-        public static double Atan(double theta, AngleUnit unit)
-        {
-            return Atan(theta) / unit.ToRad();
-        }
-
-        /// <summary>
-        /// arcus cosecant
-        /// </summary>
-        public static double Acsc(double theta, AngleUnit unit)
-        {
-            return Acsc(theta) / unit.ToRad();
-        }
-
-        /// <summary>
-        /// arcus secant
-        /// </summary>
-        public static double Asec(double theta, AngleUnit unit)
-        {
-            return Asec(theta) / unit.ToRad();
-        }
-
-        /// <summary>
         /// arcus cotangent
         /// </summary>
-        public static double Acot(double theta, AngleUnit unit)
+        public static double Acot(double theta, AngleUnit angle)
         {
-            return Acot(theta) / unit.ToRad();
+            return Acot(theta) / angle.ToRad();
         }
 
         /// <summary>
@@ -211,11 +210,27 @@ namespace ByteBee.Core.Numerics
         {
             return Math.Sinh(theta);
         }
-
+        
+        /// <summary>
+        /// Sinus hyperbolicus
+        /// </summary>
+        public static double Sinh(double theta, AngleUnit angle)
+        {
+            return Math.Sinh(theta);
+        }
+        
         /// <summary>
         /// Kosinus hyperbolicus
         /// </summary>
         public static double Cosh(double theta)
+        {
+            return Math.Cosh(theta);
+        }
+
+        /// <summary>
+        /// Kosinus hyperbolicus
+        /// </summary>
+        public static double Cosh(double theta, AngleUnit angle)
         {
             return Math.Cosh(theta);
         }
@@ -229,9 +244,25 @@ namespace ByteBee.Core.Numerics
         }
 
         /// <summary>
+        /// Tangens hyperbolicus
+        /// </summary>
+        public static double Tanh(double theta, AngleUnit angle)
+        {
+            return Math.Tanh(theta);
+        }
+
+        /// <summary>
         /// Kotangens hyperbolicus
         /// </summary>
         public static double Coth(double theta)
+        {
+            return (Math.Exp(theta) + Math.Exp(-theta)) / (Math.Exp(theta) - Math.Exp(-theta));
+        }
+
+        /// <summary>
+        /// Kotangens hyperbolicus
+        /// </summary>
+        public static double Coth(double theta, AngleUnit angle)
         {
             return (Math.Exp(theta) + Math.Exp(-theta)) / (Math.Exp(theta) - Math.Exp(-theta));
         }
@@ -245,9 +276,25 @@ namespace ByteBee.Core.Numerics
         }
 
         /// <summary>
+        /// Sekans hyperbolicus
+        /// </summary>
+        public static double Sech(double theta, AngleUnit angle)
+        {
+            return 2 / (Math.Exp(theta) + Math.Exp(theta));
+        }
+
+        /// <summary>
         /// Kosekans hyperbolicus
         /// </summary>
         public static double Csch(double theta)
+        {
+            return 2 / (Math.Exp(theta) - Math.Exp(theta));
+        }
+
+        /// <summary>
+        /// Kosekans hyperbolicus
+        /// </summary>
+        public static double Csch(double theta, AngleUnit angle)
         {
             return 2 / (Math.Exp(theta) - Math.Exp(theta));
         }
@@ -261,9 +308,25 @@ namespace ByteBee.Core.Numerics
         }
 
         /// <summary>
+        /// areasinus hyperbolicus
+        /// </summary>
+        public static double Asinh(double theta, AngleUnit angle)
+        {
+            return Math.Log(theta + Math.Sqrt(theta * theta + 1));
+        }
+
+        /// <summary>
         /// areakosinus hyperbolicus
         /// </summary>
         public static double Acosh(double theta)
+        {
+            return Math.Log(theta + Math.Sqrt(theta * theta - 1));
+        }
+
+        /// <summary>
+        /// areakosinus hyperbolicus
+        /// </summary>
+        public static double Acosh(double theta, AngleUnit angle)
         {
             return Math.Log(theta + Math.Sqrt(theta * theta - 1));
         }
@@ -277,9 +340,25 @@ namespace ByteBee.Core.Numerics
         }
 
         /// <summary>
+        /// areatangens hyperbolicus
+        /// </summary>
+        public static double Atanh(double theta, AngleUnit angle)
+        {
+            return Math.Log((1 + theta) / (1 - theta)) / 2;
+        }
+
+        /// <summary>
         /// areakotangens hyperbolicus
         /// </summary>
         public static double Acoth(double theta)
+        {
+            return Math.Log((theta + 1) / (theta - 1)) / 2;
+        }
+
+        /// <summary>
+        /// areakotangens hyperbolicus
+        /// </summary>
+        public static double Acoth(double theta, AngleUnit angle)
         {
             return Math.Log((theta + 1) / (theta - 1)) / 2;
         }
@@ -293,6 +372,14 @@ namespace ByteBee.Core.Numerics
         }
 
         /// <summary>
+        /// areasekans hyperbolicus
+        /// </summary>
+        public static double Asech(double theta, AngleUnit angle)
+        {
+            return Math.Log((Math.Sqrt(-theta * theta + 1) + 1) / theta);
+        }
+
+        /// <summary>
         /// areakosekans hyperbolicus
         /// </summary>
         public static double Acsch(double theta)
@@ -300,5 +387,12 @@ namespace ByteBee.Core.Numerics
             return Math.Log((Math.Sign(theta) * Math.Sqrt(theta * theta + 1) + 1) / theta);
         }
 
+        /// <summary>
+        /// areakosekans hyperbolicus
+        /// </summary>
+        public static double Acsch(double theta, AngleUnit angle)
+        {
+            return Math.Log((Math.Sign(theta) * Math.Sqrt(theta * theta + 1) + 1) / theta);
+        }
     }
 }
